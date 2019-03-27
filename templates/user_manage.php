d43224f901f4a2177f9fa5a6be09da4dcca99622
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-03-27 11:17:35 +0000

    Syntax error fixed

--- templates/user_manage.php
+++ templates/user_manage.php
@@ -16,15 +16,15 @@ $user_query = new WP_User_Query( $args );
 if ( ! empty( $user_query->get_results() ) ) {
 	foreach ( $user_query->get_results() as $user ) {
         $message  = 'Dear member,' . "\r\n\r\n";
-        $message  = '' . "\r\n\r\n";
+        $message .= '' . "\r\n\r\n";
         $message .= 'Welcome to the new official website of China Chamber of Commerce in Australia!' . "\r\n\r\n";
-        $message  = '' . "\r\n\r\n";
+        $message .= '' . "\r\n\r\n";
         $message .= 'Your member account has been created with email address ' . $user->user_login . ' as the login user name.' . "\r\n\r\n";
         $message .= 'Please click on the following link and follow the process to select your password.' . "\r\n\r\n";
         $message .= site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user->user_login ), 'login' ) . "\r\n\r\n";
-        $message  = '' . "\r\n\r\n";
+        $message .= '' . "\r\n\r\n";
         $message .= 'Thanks for your support.' . "\r\n\r\n";
-        $message  = '' . "\r\n\r\n";
+        $message .= '' . "\r\n\r\n";
         $message .= 'CCCA team' . "\r\n";
         $subject = 'Your CCCA member account is ready.';
         echo $message;
