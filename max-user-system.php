c22b34cbab1d538a2e99b88b837634699e1e788e
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-03-27 11:47:23 +0000

    Add some space

--- max-user-system.php
+++ max-user-system.php
@@ -551,12 +551,14 @@ class Personalize_Login_Plugin {
     public function replace_retrieve_password_message( $message, $key, $user_login, $user_data ) {
         // Create new message
         $msg  = __( 'Dear member,', 'max-user' ) . "\r\n\r\n";
+        $msg .= '' . "\r\n\r\n";
         $msg .= sprintf( __( 'You have requested to reset your password for your account %s.', 'max-user' ), $user_login ) . "\r\n\r\n";
         $msg .= __( "If the request is not from you, please ignore this email.", 'max-user' ) . "\r\n\r\n";
         $msg .= __( 'To reset your password, please click on the following link and follow the process to select your password:', 'max-user' ) . "\r\n\r\n";
         $msg .= site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user_login ), 'login' ) . "\r\n\r\n";
+        $msg .= '' . "\r\n\r\n";
         $msg .= __( 'Thanks for your support.', 'max-user' ) . "\r\n\r\n";
-        $msg .= __( 'Thanks for your support.', 'max-user' ) . "\r\n";
+        $msg .= __( 'CCCA team ', 'max-user' ) . "\r\n";
 
         return $msg;
     }
