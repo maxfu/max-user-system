5d9532fce09023c4d04094ddf27b3160f4fd446b
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-04-08 12:47:26 +0000

    Fix

--- templates/user_manage.php
+++ templates/user_manage.php
@@ -8,31 +8,31 @@ $user_query = new WP_User_Query( $args );
 
 // The User Loop
 if ( ! empty( $user_query->results ) ) { ?>
-<h2 class="mbr-section-subtitle mbr-fonts-style align-center pb-5 mbr-light display-5"><?php _e( 'CCCA Member Profile', 'max-user' ); ?> <?php echo $user_info->user_login; ?></h2>
+<h2 class="mbr-section-subtitle mbr-fonts-style align-center pb-5 mbr-light display-5"><?php _e( 'CCCA Member List', 'max-user' ); ?> <?php echo $user_info->user_login; ?></h2>
 <table class="form-table table isSearch" id="ccca-member-profile">
   <tbody>
         <tr>
-            <th class="body-item mbr-fonts-style display-7"><?php _e( 'Company Name English', 'max-user' ); ?></td>
-            <th class="body-item mbr-fonts-style display-7"><?php _e( 'Company Name Chinese', 'max-user' ); ?></td>
-            <th class="body-item mbr-fonts-style display-7"><?php _e( 'Company Email', 'max-user' ); ?></td>
-            <th class="body-item mbr-fonts-style display-7"><?php _e( 'Company Website', 'max-user' ); ?></th>
-            <th class="body-item mbr-fonts-style display-7"><?php _e( 'Company Address', 'max-user' ); ?></td>
-            <th class="body-item mbr-fonts-style display-7"><?php _e( 'Company Telephone', 'max-user' ); ?></td>
-            <th class="body-item mbr-fonts-style display-7"><?php _e( 'Second Email', 'max-user' ); ?></td>
-            <th class="body-item mbr-fonts-style display-7"><?php _e( 'Third Email', 'max-user' ); ?></td>
-            <th class="body-item mbr-fonts-style display-7"><?php _e( 'Forth Email', 'max-user' ); ?></td>
+            <th><?php _e( 'Company Name English', 'max-user' ); ?></td>
+            <th><?php _e( 'Company Name Chinese', 'max-user' ); ?></td>
+            <th><?php _e( 'Company Email', 'max-user' ); ?></td>
+            <th><?php _e( 'Company Website', 'max-user' ); ?></th>
+            <th><?php _e( 'Company Address', 'max-user' ); ?></td>
+            <th><?php _e( 'Company Telephone', 'max-user' ); ?></td>
+            <th><?php _e( 'Second Email', 'max-user' ); ?></td>
+            <th><?php _e( 'Third Email', 'max-user' ); ?></td>
+            <th><?php _e( 'Forth Email', 'max-user' ); ?></td>
         </tr>
 <?php foreach ( $user_query->results as $user ) { ?>
         <tr>
-            <td class="body-item mbr-fonts-style display-7"><?php echo $user->first_name; ?></td>
-            <td class="body-item mbr-fonts-style display-7"><?php echo $user->last_name; ?></td>
-            <td class="body-item mbr-fonts-style display-7"><?php echo $user->company_email; ?></td>
-            <td class="body-item mbr-fonts-style display-7"><?php echo $user->user_url; ?></td>
-            <td class="body-item mbr-fonts-style display-7"><?php echo $user->company_address; ?></td>
-            <td class="body-item mbr-fonts-style display-7"><?php echo $user->company_phone; ?></td>
-            <td class="body-item mbr-fonts-style display-7"><?php echo $user->second_email; ?></td>
-            <td class="body-item mbr-fonts-style display-7"><?php echo $user->third_email; ?></td>
-            <td class="body-item mbr-fonts-style display-7"><?php echo $user->forth_email; ?></td>
+            <td><?php echo $user->first_name; ?></td>
+            <td><?php echo $user->last_name; ?></td>
+            <td><?php echo $user->company_email; ?></td>
+            <td><?php echo $user->user_url; ?></td>
+            <td><?php echo $user->company_address; ?></td>
+            <td><?php echo $user->company_phone; ?></td>
+            <td><?php echo $user->second_email; ?></td>
+            <td><?php echo $user->third_email; ?></td>
+            <td><?php echo $user->forth_email; ?></td>
         </tr>
 	<?php } ?>
   </tbody>
